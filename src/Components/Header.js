37c22cd704591ffd80b711d4e34ef74d36f9ac8b
Hemant Reddy <hemantreddy22@gmail.com>
2019-07-03 15:06:10 +0530

    modified header component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,14 @@
 import React from 'react'
 
-let Header = () => {
-    return (
-        <p>Header component</p>
+function Header(){
+    return(
+        <header>
+            <img 
+                src = "http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" 
+                alt = "problem?"
+            />
+            <p>Meme Generator</p>
+        </header>
     )
 }
 
